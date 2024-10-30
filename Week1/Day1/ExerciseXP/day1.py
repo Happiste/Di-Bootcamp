e4303083609ec4de90149c6f218b96be2d585df6
John <jonathan.bitane@gmail.com>
2024-10-30 10:57:42 +0200

    Day1

---
+++ Week1/Day1/ExerciseXP/day1.py
@@ -0,0 +1,18 @@
+#Exo 1 
+string = "Hello world\n"
+print(string*4)
+
+#Exo 2
+result = (99**3)*8
+print(result)
+
+#Exo 3 
+# 5 < 3 = False
+# 3 == 3 = True
+# 3 == "3" = False
+# "3" > 3 = False
+# "Hello" == "hello" = False
+
+#Exo 4
+brand_computer = "Macbook Pro, intel"
+print(f"i have a {brand_computer}  computer")
