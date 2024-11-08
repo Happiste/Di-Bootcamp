d5c9a0737647ad82ae2ba0db06f13d1b420a5747
John <jonathan.bitane@gmail.com>
2024-11-08 13:08:28 +0200

    DailyChallenge

---
+++ Week1/Day5/DailyChallenge/DailyChallenge.py
@@ -0,0 +1,26 @@
+# #Challenge 1 Sorting
+
+# string = "without,hello,bag,world"
+# w_comma = string.split(',')
+# sort_lst = sorted(w_comma)
+# string_sorted = ",".join(sort_lst)
+# print(string_sorted)
+
+
+#Challenge 2 Longest Word
+
+def longest_word(string=input("Please enter a sentences: " )):
+    lst_words = string.split()
+    longest = ""
+    for index, i in enumerate(lst_words):
+        length_word = len(i)
+        if len(longest) < length_word:
+            longest = i 
+        else:
+            continue
+        #print(f"i = {i}, index = {index}, length_word = {length_word}")
+    print(lst_words)
+    return(longest)
+
+print(longest_word())
+
