71c70d5f4db2f776bb62e3bfc4301acfee8da302
John <jonathan.bitane@gmail.com>
2024-11-10 10:43:41 +0200

    DailyChallenge

--- Week2/Day1/ExerciseXP/ExcerciceXP_GOLD.py
+++ Week2/Day1/ExerciseXP/ExcerciceXP_GOLD.py
@@ -1,67 +1,67 @@
-# #Excercice XP GOLD 
+#Excercice XP GOLD 
 
-# #Exo 1
+#Exo 1
 
-# #perimetre = 2*pi*rayon
-# #air = pi*(radius**2)
-# import math 
+#perimetre = 2*pi*rayon
+#air = pi*(radius**2)
+import math 
 
-# class Circle:
-#     def __init__(self, radius=1.0):
-#         self.radius = radius
+class Circle:
+    def __init__(self, radius=1.0):
+        self.radius = radius
 
-#     def get_perimeter(self):
-#         perimeter = 2*math.pi*self.radius
-#         return(perimeter)
+    def get_perimeter(self):
+        perimeter = 2*math.pi*self.radius
+        return(perimeter)
     
-#     def get_area(self):
-#         area = math.pi*(self.radius**2)
-#         return(area)
+    def get_area(self):
+        area = math.pi*(self.radius**2)
+        return(area)
     
-#     def get_def(self):
-#         p = self.get_perimeter()
-#         a = self.get_area()
-#         print(f"un cercle de rayon {self.radius} à un perimétre de {round(p)} cm, et une aire de {round(a)}cm carrée")
+    def get_def(self):
+        p = self.get_perimeter()
+        a = self.get_area()
+        print(f"un cercle de rayon {self.radius} à un perimétre de {round(p)} cm, et une aire de {round(a)}cm carrée")
 
-# my_circle = Circle(5.0)
-# my_circle.get_perimeter()
-# my_circle.get_area()
-# my_circle.get_def()
+my_circle = Circle(5.0)
+my_circle.get_perimeter()
+my_circle.get_area()
+my_circle.get_def()
 
 
-# #Exo 2
-# import random as r
+#Exo 2
+import random as r
 
-# class Mylist:
-#     def __init__(self, lst):
-#         self.lst = lst
+class Mylist:
+    def __init__(self, lst):
+        self.lst = lst
 
-#     def reversed_lst(self):
-#         list_reversed = self.lst[::-1]
-#         return(list_reversed)
+    def reversed_lst(self):
+        list_reversed = self.lst[::-1]
+        return(list_reversed)
     
-#     def sorted_list(self):
-#         sorted_list = sorted(self.lst)
-#         return(sorted_list)
+    def sorted_list(self):
+        sorted_list = sorted(self.lst)
+        return(sorted_list)
     
-#     def copy_len_list(self):
-#         copy_list = [r.randint(1, 10000) for _ in range(len(self.lst))]
-#         print(f"here is the lenght copy list with random numbers : {copy_list}")
-#         return(copy_list)
+    def copy_len_list(self):
+        copy_list = [r.randint(1, 10000) for _ in range(len(self.lst))]
+        print(f"here is the lenght copy list with random numbers : {copy_list}")
+        return(copy_list)
     
-#     def get_list(self):
-#         print(f"""liste d'origine: {self.lst}
-# liste reversed: {self.reversed_lst()}
-# liste sorted : {self.sorted_list()}
-# the len copied list with random num: {self.copy_len_list()}""")
+    def get_list(self):
+        print(f"""liste d'origine: {self.lst}
+liste reversed: {self.reversed_lst()}
+liste sorted : {self.sorted_list()}
+the len copied list with random num: {self.copy_len_list()}""")
 
     
-# lst = [5,12,7,78,1,87,13,6]
-# my_list = Mylist(lst)
-# my_list.reversed_lst()
-# my_list.sorted_list()
-# my_list.get_list()
-# my_list.copy_len_list
+lst = [5,12,7,78,1,87,13,6]
+my_list = Mylist(lst)
+my_list.reversed_lst()
+my_list.sorted_list()
+my_list.get_list()
+my_list.copy_len_list
 
 
 
--- Week2/Day1/ExerciseXP/ExerciceXP.py
+++ Week2/Day1/ExerciseXP/ExerciceXP.py
@@ -1,138 +1,138 @@
-# #EXO 1
-# class Cat:
-#     def __init__(self, cat_name, cat_age):
-#         self.name = cat_name
-#         self.age = cat_age
+#EXO 1
+class Cat:
+    def __init__(self, cat_name, cat_age):
+        self.name = cat_name
+        self.age = cat_age
 
-# shanks = Cat("shanks", 5)
-# Buggy = Cat("Buggy", 1)
-# Yuna = Cat("Yuna", 3)
+shanks = Cat("shanks", 5)
+Buggy = Cat("Buggy", 1)
+Yuna = Cat("Yuna", 3)
 
-# def oldest_cat(*cats):
-#     oldest_cat = cats[0]
-#     for cat in cats:
-#         if cat.age > oldest_cat.age:
-#             oldest_cat = cat
-#     return(oldest_cat)
+def oldest_cat(*cats):
+    oldest_cat = cats[0]
+    for cat in cats:
+        if cat.age > oldest_cat.age:
+            oldest_cat = cat
+    return(oldest_cat)
 
-# oldest = oldest_cat(shanks, Buggy, Yuna)
-# print(oldest.name, oldest.age)
+oldest = oldest_cat(shanks, Buggy, Yuna)
+print(oldest.name, oldest.age)
 
-# #Exo 2
+#Exo 2
  
-# class Dog:
-#     def __init__(self, name, height, owner):
-#         self.name = name
-#         self.height = height
-#         self.owner = owner
+class Dog:
+    def __init__(self, name, height, owner):
+        self.name = name
+        self.height = height
+        self.owner = owner
 
-#     def bark(self):
-#         print(f"{self.name} goes woof!")
+    def bark(self):
+        print(f"{self.name} goes woof!")
 
-#     def jump(self):
-#         x = self.height * 2
-#         print(f"{self.name} jumps {x} cm height ")
+    def jump(self):
+        x = self.height * 2
+        print(f"{self.name} jumps {x} cm height ")
     
-#     def details_dog(self):
-#         print(f"The name of {self.owner}'s dog is {self.name} and is height is {self.height}!")
+    def details_dog(self):
+        print(f"The name of {self.owner}'s dog is {self.name} and is height is {self.height}!")
 
-# def whos_bigger(*dogs):
-#     bigger = dogs[0]
-#     for dog in dogs:
-#         if dog.height > bigger.height:
-#             bigger = dog
-#     return bigger
+def whos_bigger(*dogs):
+    bigger = dogs[0]
+    for dog in dogs:
+        if dog.height > bigger.height:
+            bigger = dog
+    return bigger
 
 
 
-# davids_dog = Dog("Happy", 60, "David")
-# davids_dog.details_dog()
-# davids_dog.bark()
-# davids_dog.jump()
+davids_dog = Dog("Happy", 60, "David")
+davids_dog.details_dog()
+davids_dog.bark()
+davids_dog.jump()
 
-# sarahs_dog = Dog("Teacup", 20, 'Sarah')
-# sarahs_dog.details_dog()
-# sarahs_dog.bark()
-# sarahs_dog.jump()
+sarahs_dog = Dog("Teacup", 20, 'Sarah')
+sarahs_dog.details_dog()
+sarahs_dog.bark()
+sarahs_dog.jump()
 
-# my_dog = Dog("Taz", 66, "John")
-# my_dog.details_dog
-# my_dog.bark()
-# my_dog.jump()
+my_dog = Dog("Taz", 66, "John")
+my_dog.details_dog
+my_dog.bark()
+my_dog.jump()
 
-# biggest = whos_bigger(davids_dog, sarahs_dog, my_dog)
-# print(f"the biggest dog is {biggest.owner}'s dog: {biggest.name}")
+biggest = whos_bigger(davids_dog, sarahs_dog, my_dog)
+print(f"the biggest dog is {biggest.owner}'s dog: {biggest.name}")
 
-# #Exo 3
-# class Song:
-#     def __init__(self, lst_lyrics):
-#         self.lst_lyrics = lst_lyrics
+#Exo 3
+class Song:
+    def __init__(self, lst_lyrics):
+        self.lst_lyrics = lst_lyrics
 
-#     def sing_me_a_song(self):
-#         for line in self.lst_lyrics:
-#             print(line)
+    def sing_me_a_song(self):
+        for line in self.lst_lyrics:
+            print(line)
 
-# stairway= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
+stairway= Song(["There’s a lady who's sure","all that glitters is gold", "and she’s buying a stairway to heaven"])
 
-# stairway.sing_me_a_song()
+stairway.sing_me_a_song()
 
-# #Exo 4
-# class Zoo:
-#     def __init__(self, zoo_name):
-#         self.zoo_name = zoo_name
-#         self.animals = []
+#Exo 4
+class Zoo:
+    def __init__(self, zoo_name):
+        self.zoo_name = zoo_name
+        self.animals = []
 
-#     def add_animal(self, new_animal):
-#         if new_animal in self.animals:
-#             print("we already have this animals")
-#         else:
-#             self.animals.append(new_animal)
-#             print(f"A new animal: {new_animal} has join the family of {self.zoo_name}  ")
+    def add_animal(self, new_animal):
+        if new_animal in self.animals:
+            print("we already have this animals")
+        else:
+            self.animals.append(new_animal)
+            print(f"A new animal: {new_animal} has join the family of {self.zoo_name}  ")
 
-#     def get_animals(self):
-#         print(f"This Zoo is called: {self.zoo_name} and has: {self.animals} animals")
+    def get_animals(self):
+        print(f"This Zoo is called: {self.zoo_name} and has: {self.animals} animals")
 
-#     def sell_animal(self, animal_sold):
-#         if animal_sold in self.animals:
-#             self.animals.remove(animal_sold)
-#             print(f"the {animal_sold} has been sold")
-#         else:
-#             print("we can't sell what we don't have ! ")
+    def sell_animal(self, animal_sold):
+        if animal_sold in self.animals:
+            self.animals.remove(animal_sold)
+            print(f"the {animal_sold} has been sold")
+        else:
+            print("we can't sell what we don't have ! ")
 
-#     def sorted_animals(self):
-#         animal_sorted = sorted(self.animals)
-#         dico = {} 
-#         for animal in animal_sorted:
-#             first_letter = animal[0]
-#             if first_letter not in dico:
-#                 dico[first_letter] = []
-#             dico[first_letter].append(animal) 
-#         grouped_animals = {}
-#         group_number = 1
-#         for letter in dico:
-#             grouped_animals[group_number] = dico[letter]
-#             group_number +=1
-#         return(grouped_animals)
+    def sorted_animals(self):
+        animal_sorted = sorted(self.animals)
+        dico = {} 
+        for animal in animal_sorted:
+            first_letter = animal[0]
+            if first_letter not in dico:
+                dico[first_letter] = []
+            dico[first_letter].append(animal) 
+        grouped_animals = {}
+        group_number = 1
+        for letter in dico:
+            grouped_animals[group_number] = dico[letter]
+            group_number +=1
+        return(grouped_animals)
   
                 
-#     def get_groups(self):
-#         sorted = self.sorted_animals()
-#         print(f"here is the grouped animals: {sorted}")
+    def get_groups(self):
+        sorted = self.sorted_animals()
+        print(f"here is the grouped animals: {sorted}")
 
                            
 
-# my_zoo = Zoo("John's Zoo")
-# my_zoo.add_animal("Tiger")
-# my_zoo.add_animal("Lion")
-# my_zoo.get_animals()
-# my_zoo.sell_animal("Lion")
-# my_zoo.get_animals()
-# my_zoo.add_animal("Tiger")
-# my_zoo.add_animal("Lion")
-# my_zoo.add_animal("Levrier")
-# my_zoo.sell_animal("Cheval")
-# my_zoo.add_animal("Horse")
-# my_zoo.get_animals()
-# my_zoo.sorted_animals()
-# my_zoo.get_groups()
+my_zoo = Zoo("John's Zoo")
+my_zoo.add_animal("Tiger")
+my_zoo.add_animal("Lion")
+my_zoo.get_animals()
+my_zoo.sell_animal("Lion")
+my_zoo.get_animals()
+my_zoo.add_animal("Tiger")
+my_zoo.add_animal("Lion")
+my_zoo.add_animal("Levrier")
+my_zoo.sell_animal("Cheval")
+my_zoo.add_animal("Horse")
+my_zoo.get_animals()
+my_zoo.sorted_animals()
+my_zoo.get_groups()
 
