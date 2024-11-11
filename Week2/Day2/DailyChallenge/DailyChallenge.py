57df21533f88aa784bc8f0f31e9932035b7f0254
John <jonathan.bitane@gmail.com>
2024-11-11 16:24:10 +0200

    DailyChallenge.py

---
+++ Week2/Day2/DailyChallenge/DailyChallenge.py
@@ -0,0 +1,44 @@
+class Pagination:
+    def __init__(self, items=None, pageSize=10):
+        self.items = items
+        self.pageSize = pageSize
+        self.currentPage = 1
+
+    def getVisibleItems(self):
+
+        start= (self.currentPage - 1) * self.pageSize
+        ending = self.currentPage * self.pageSize
+        print(self.items[start:ending])
+
+    def next_page(self):
+        self.currentPage += 1
+
+    def prev_page(self):
+        self.currentPage -= 1
+
+    def first_page(self):
+        self.currentPage = 1
+    
+    def last_page(self):
+        pass
+
+    def goToPage(self, num=1):
+        self.currentPage = num
+        
+
+
+if __name__=='__main__':
+    alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+    p = Pagination(alphabetList, 4)
+    p.getVisibleItems()
+    p.next_page()
+    p.getVisibleItems()
+    p.next_page()
+    p.getVisibleItems()
+    p.prev_page()
+    p.getVisibleItems()
+    p.first_page()
+    p.getVisibleItems()
+    #p.last_page()
+    p.goToPage(7)
+    p.getVisibleItems()
