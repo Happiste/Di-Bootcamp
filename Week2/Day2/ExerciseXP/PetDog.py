975907dd6473106e51338c2eef61bd5e0f18a5f5
John <jonathan.bitane@gmail.com>
2024-11-11 14:35:47 +0200

    ExcerciceXP finished

--- Week2/Day2/ExerciseXP/PetDog.py
+++ Week2/Day2/ExerciseXP/PetDog.py
@@ -1,31 +1,31 @@
-#Exo 3 Dogs Domesticated
+# #Exo 3 Dogs Domesticated
 
-import ExcerciceXP as x
-import random as r
+# import ExcerciceXP as x
+# import random as r
 
-class PetDog(x.Dog):
-    def __init__(self, name, age, weight):
-        super().__init__(name, age, weight)
-        self.trained = False
+# class PetDog(x.Dog):
+#     def __init__(self, name, age, weight):
+#         super().__init__(name, age, weight)
+#         self.trained = False
 
-    def train(self):
-        print(self.bark())
-        self.trained = True
+#     def train(self):
+#         print(self.bark())
+#         self.trained = True
 
-    def play(self, *args):
-        dogs = ""
-        for arg in args:
-            dogs += " " + arg.name
-        dog_names = self.name + dogs
-        print(f"{dog_names} all play together")
+#     def play(self, *args):
+#         dogs = ""
+#         for arg in args:
+#             dogs += " " + arg.name
+#         dog_names = self.name + dogs
+#         print(f"{dog_names} all play together")
 
-    def do_a_trick(self):
-        lst_action = [" does a barrel roll", " stand on his back legs", " shake your hand", " plays dead"]
-        action = r.choice(lst_action)
-        if self.trained == True:
-            print(f"{self.name}{action}")
-        else: 
-            print(f"your dog {self.name} is not trained yet he just can bark")
+#     def do_a_trick(self):
+#         lst_action = [" does a barrel roll", " stand on his back legs", " shake your hand", " plays dead"]
+#         action = r.choice(lst_action)
+#         if self.trained == True:
+#             print(f"{self.name}{action}")
+#         else: 
+#             print(f"your dog {self.name} is not trained yet he just can bark")
             
             
 
@@ -33,7 +33,7 @@ class PetDog(x.Dog):
         
 
 
-his_dog = PetDog("Edouard", 8, 35)
-his_dog.train()
-his_dog.play(x.my_dog, x.your_dog)
-his_dog.do_a_trick()
+# his_dog = PetDog("Edouard", 8, 35)
+# his_dog.train()
+# his_dog.play(x.my_dog, x.your_dog)
+# his_dog.do_a_trick()
