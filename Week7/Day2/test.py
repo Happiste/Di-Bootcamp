233d63bd8dfd09152cff05707675f4731f4f1eb6
John <jonathan.bitane@gmail.com>
2024-12-24 09:29:44 +0100

    all

--- Week7/Day2/test.py
+++ Week7/Day2/test.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 # Remplacez le chemin par celui où votre chromedriver est installé, si nécessaire
-service = Service("/path/to/chromedriver")
+service = Service("/usr/local/bin/chromedriver")
 
 driver = webdriver.Chrome(service=service)
 driver.get("https://www.google.com")
 print(driver.title)
-driver.quit()
+driver.quit()   
